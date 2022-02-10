68a6cc55384c19a4c85751fbc692955a30e858f8
KazukiNagaishi <98684762+KazukiNagaishi@users.noreply.github.com>
2022-02-10 14:13:42 +0900

    first commit

---
+++ app_chatbot.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, abort
+import os
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+from search_ans import *
+
+app = Flask(__name__)
+
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/")
+def hello_world():
+    return "hello world!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X‐Line‐Signature header value
+    signature = request.headers['X‐Line‐Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channelsecret.")
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    partner_message = event.message.text
+    reply_message = return_reply(partner_message)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_message))
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
---
+++ search_ans.py
@@ -0,0 +1,53 @@
+from transformers import BertJapaneseTokenizer, BertModel
+from sklearn.metrics.pairwise import cosine_similarity
+import torch
+
+# 学習済みBERTモデルを読込
+tokenizer = BertJapaneseTokenizer \
+    .from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
+model_bert = BertModel \
+    .from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking')
+
+
+# 埋込み表現を計算
+def calc_embedding(text):
+    bert_tokens = tokenizer.tokenize(text)
+    ids = tokenizer.convert_tokens_to_ids(['[CLS]'] + bert_tokens[:126] + ['[SEP]'])
+    tokens_tensor = torch.tensor(ids).reshape(1, -1)
+    with torch.no_grad():
+        output = model_bert(tokens_tensor)
+
+    return output[1].numpy()
+
+
+# コサイン類似度を計算
+def calc_cosine_similarity(text1, text2):
+    w1 = calc_embedding(text1)
+    w2 = calc_embedding(text2)
+    return cosine_similarity(w1, w2)[0, 0]
+
+# Main
+def return_reply(message):
+    sentences = []
+    similarities = []
+
+    query = message
+
+    f = open('Data/data.txt', 'r', encoding='UTF-8')
+    while True:
+      data = f.readline().strip()
+      if data:
+          sentences.append(data.split(','))
+      else:
+          break
+    f.close()
+
+    for i, data in enumerate(sentences):
+        sim = calc_cosine_similarity(query, data[0])
+        similarities.append([sim, i, data[1]])
+
+    similarities.sort(reverse=True)
+
+    return similarities[0][2]
+
+    similarities.clear()
